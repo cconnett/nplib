02ff3cb65e1fe66560e0e0bd2987c3748f984306
Chris Connett <chris@connett.net>
2008-05-10 13:22:34 -0400

    Add writeRepo function to parseData module.

--- parseData.py
+++ parseData.py
@@ -17,3 +17,10 @@ def missing(repo):
     for num in range(1,1001):
         if (num, 'lower') not in repo or (num, 'upper') not in repo:
             yield num
+
+def writeRepo(repo, filename):
+    keys = repo.keys()
+    keys.sort()
+    with file(filename, 'w') as f:
+        for (num, bound) in keys:
+            print '%s %s: %s' % (num, bound, repo[(num, bound)])
