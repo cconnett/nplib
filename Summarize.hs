b00f6bb586faf2e20266cb6fbd03fd678caa33cb
Chris Connett <chris@connett.net>
2008-07-02 01:06:00 -0400

    Taught Summarize an electionNo parameter to select an election from a file.

--- Summarize.hs
+++ Summarize.hs
@@ -6,15 +6,16 @@ import Data.List
 
 main = do
   args <- getArgs
-  if length args /= 1
-     then error "Summarize electionsFile"
+  if length args /= 2
+     then error "Summarize electionsFile electionNo"
      else do
        let electionsFilename = (args !! 0) :: String
+           electionNo = read (args !! 1) :: Int
        electionsData <- readFile electionsFilename
        let elections = (read $ electionsData) :: [[Vote Int]]
            candidates = extractCandidates (head elections)
        mapM (\candidate -> putStrLn $ show $ scoringProtocolScore bordaS candidates (head elections) (Candidate candidate)) [1..length candidates]
-       putStr $ unlines $ map ((unwords.(map (show.fromCandidate))).fromVote) (head elections)
+       putStr $ unlines $ map ((unwords.(map (show.fromCandidate))).fromVote) (elections !! (electionNo - 1))
 
 scoringProtocolScore s candidates votes candidate = (candidate, score candidate)
     where score candidate = sum $ map (scoreList!!) $ mapMaybe (\vote -> (candidate `elemIndex` (fromVote vote))) votes
