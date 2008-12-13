f60c45a85677c0c1c561d4e298c73873aef7b2b4
Chris Connett <chris@connett.net>
2008-12-12 22:59:55 -0500

    Improve status printing of generated TestAll program.

--- constructTestMain.py
+++ constructTestMain.py
@@ -1,3 +1,4 @@
+import math
 import sys
 import re
 
@@ -25,14 +26,19 @@ sys.stdout = file(sys.argv[1], 'w')
 
 print 'module Main where'
 print
+print 'import IO'
 print 'import Test.QuickCheck'
 print
 for i in imports:
     print 'import', i
 print
 print 'main = do'
-for prop in props:
-    print '  putStr "%s: "' % prop
+print '  hSetBuffering stdout NoBuffering'
+if len(props) > 0:
+    numDigits = int(math.log(len(props)))
+    statusString = '  putStr "[%%%dd of %%d] %%s: "' % numDigits
+for (n, prop) in enumerate(props):
+    print statusString % (n+1, len(props), prop)
     print '  quickCheck %s' % prop
 
 sys.stdout.close()
