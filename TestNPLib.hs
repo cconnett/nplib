85cfc2d9991dda7b3e6bf9ee61509fc9c0cb491c
Chris Connett <chris@connett.net>
2008-12-12 19:33:46 -0500

    Add two new properties that test Embeddings.

--- TestNPLib.hs
+++ TestNPLib.hs
@@ -1,12 +1,15 @@
 module TestNPLib where
 
+import Data.Maybe
 import Elections
 import Embeddings
-import SatSolvers
-import SAT
-import Utilities
+import NInteger
 import NProgram
 import NVar
+import SAT
+import SatSolvers
+import Solving
+import Utilities
 import Voting hiding (beats)
 
 import Test.QuickCheck
@@ -14,3 +17,27 @@ import Test.QuickCheck
 mode = RSat
 
 prop_doubleNegation prop = prop == (neg $ neg $ prop)
+
+prop_if' a b =
+    (a==b) == (snd $
+               evalNProgram Minisat (do
+                                      cond::Var <- new
+                                      let a'::NInteger = NInteger.fromInteger a
+                                      let b'::NInteger = NInteger.fromInteger b
+                                      eq <- a'`equal`b'
+                                      if' cond
+                                          (eq)
+                                          (emptyFormula)
+                                      if' eq
+                                         (emptyFormula)
+                                         (makeFalse cond)
+                                      return cond)
+              )
+prop_deny a b =
+    (a/=b) == (fromJust $
+               execNProgram Minisat (do
+                                      let a'::NInteger = NInteger.fromInteger a
+                                      let b'::NInteger = NInteger.fromInteger b
+                                      eq <- a'`equal`b'
+                                      deny eq)
+              )
