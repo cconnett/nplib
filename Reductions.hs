3dc093f0bc075c09220a0add60750ae8e62f22bd
Chris Connett <chris@connett.net>
2009-02-20 02:27:40 -0500

    Rewrite irvManipulation in the new monadic style.

--- Reductions.hs
+++ Reductions.hs
@@ -20,7 +20,7 @@ instance Read (ManipulationProblem)  where
     readsPrec _ "pluralityWithRunoff" = [(pluralityWithRunoffManipulation, "")]
     readsPrec _ "borda" = [(scoringProtocolManipulation (\n -> [n-1,n-2..0]), "")]
     readsPrec _ "veto" = [(scoringProtocolManipulation (\n -> replicate (n-1) 1 ++ [0]), "")]
---    readsPrec _ "irv" = [(irvManipulation, "")]
+    readsPrec _ "irv" = [(irvManipulation, "")]
 --    readsPrec _ "copeland" = [(copelandManipulation (1%2), "")]
     readsPrec _ _ = error $ "Supported rules are\nplurality\npluralityWithRunoff\nborda\nveto\nirv\ncopeland\n"
 
@@ -78,56 +78,63 @@ pluralityWithRunoffManipulation votes numManipulators target =
         advances <- embedFormula $ fromListForm [[Not loss] | loss <- losses]
         ntrace (show c ++ " advances") advances (show::Bool->String)
         assert $ makeOpposed advances (eliminations ! (2, c))
+
       -- Target candidate still remains in round 2, with everyone else
       -- eliminated, and therefore wins.
-
       forM_ candidates $ \c -> do
         assert $ fromListForm [[(if c == target then neg else id) $
                                 Merely $ eliminations ! (2, c)]]
 
+
+irvManipulation votes numManipulators target =
+  let numNonmanipulators = length votes
+      candidates = sort $ extractCandidates votes
+      candRange = (head candidates, last candidates)
+      numCandidates = length candidates
+      rounds = [0 .. length candidates - 1] in
+    do
+      ballots <- makePairwiseBallots votes candRange numManipulators
+      eliminations <- makeEliminations rounds candidates
+      firstPlaceScores <- getFirstPlaceScores ballots candRange eliminations rounds
+
+      ntrace "FirstPlaceScores" firstPlaceScores (show::Array (Round, Candidate Int) Integer -> String)
+      ntrace "EliminationData" eliminations $ myTrace 1 (show numManipulators ++ " manipulators for " ++ show target) showEliminationData
+      manipulatorPairwiseBeatsASAR (drop numNonmanipulators ballots) candidates
+      manipulatorPairwiseBeatsTotal (drop numNonmanipulators ballots) candidates
+
+      -- Elimination: We protect candidates who strictly beat another
+      -- non-eliminated candidate, and also force out all candidates
+      -- who do not meet this criterion.  Both sides are needed to
+      -- deny the SAT solver any liberty in deciding who is eliminated
+      -- (by means other than selecting manipulator ballots).  For
+      -- every pair of distinct candidates in a given round where both
+      -- are still in, if one strictly beats the other, that candidate
+      -- is protected from elimination.
+      forM_ (init rounds) $ \round -> do { -- We only perform eliminations up to the last round.
+        forM_ candidates $ \candidate -> do
+          victories <- victories candidates firstPlaceScores round candidate
+          onlyOneLeft <- embedFormula $ fromListForm
+                         [[(if other == candidate then Not else Merely) $ eliminations ! (round, other)]
+                               | other <- candidates]
+          ntrace "Only one left" onlyOneLeft (show::Bool->String)
+
+          -- Advance this candidate it beat someone who's not
+          -- eliminatoed or if it's the only one left.
+          nonEliminatedVictories <- embedFormulas $
+                                    [fromListForm [[Merely victory], [Not $ eliminations ! (round, opponent)]]
+                                     | (victory, opponent) <- zip victories (delete candidate candidates)]
+          ntrace ("Round " ++ show round ++ " non-elim victories, " ++ show candidate) nonEliminatedVictories (show::[Bool]->String)
+          advances <- embedFormula $ fromListForm [map Merely $ onlyOneLeft : nonEliminatedVictories]
+          ntrace (show candidate ++ " advances") advances (show::Bool->String)
+          assert $ makeOpposed advances (eliminations ! (round+1, candidate)) }
+
+      -- Target candidate still remains after |C| - 1 rounds, with
+      -- everyone else eliminated, and therefore wins.
+      forM_ candidates $ \c -> do
+        assert $ fromListForm [[(if c == target then neg else id) $
+                                Merely $ eliminations ! (last rounds, c)]]
+
 {-
-irvManipulation votes =
-    let voterSet = [1..length votes]
-        manipulatorSet = map (+length votes) [1..length votes + 1]
-        ballots = voterSet ++ manipulatorSet
-        candidates = extractCandidates votes
-        positions = [0..length candidates - 1]
-        rounds = [0..length candidates - 1]
-        beats' = beats candidates ballots in
-    (concat [manipulatorPairwiseBeatsASAR manipulatorSet candidates,
-             manipulatorPairwiseBeatsTotal manipulatorSet candidates,
-             eliminationBasics candidates rounds,
-             firstPlacePoints candidates ballots rounds] ++
-     -- Elimination: We protect candidates who strictly beat another
-     -- non-eliminated candidate, and also force out all candidates
-     -- who do not meet this criterion.  Both sides are needed to deny
-     -- the SAT solver any liberty in deciding who is eliminated (by
-     -- means other than selecting manipulator ballots).  For every
-     -- pair of distinct candidates in a given round where both are
-     -- still in, if one strictly beats the other, that candidate is
-     -- protected from elimination.
-     concat
-     [beats' a b r $ \aBeatsB ->
-          [Formula [Clause [neg aBeatsB, neg $ Merely $ Eliminated (r+1) a]]]
-      | a <- candidates,
-        b <- candidates, a /= b,
-        r <- [0..length candidates - 2 {-we only perform eliminations up to the last round-}]] ++
-     concat
-     [allOthersEliminated candidates r c $ \aoe ->
-      victories candidates ballots r c $ \vics ->
-      shouldBeEliminated aoe vics r c $ \bShouldBeEliminated ->
-      [equivalent bShouldBeEliminated (Merely $ Eliminated (r+1) c)]
-          | c <- candidates,
-            r <- [0..length candidates - 2 {-we only perform eliminations up to the last round-}]]
-
-    , \votes manipulators target ->
-        count ballots (length votes + manipulators) ++
-        nonManipulatorPairwiseVotes votes voterSet candidates ++
-     -- Target candidate still remains after |C| - 1 rounds, with everyone else eliminated, and therefore wins
-        [Formula [Clause [(if c == target then neg else id) $ Merely $ Eliminated (length candidates - 1) c]
-                      | c <- candidates ]]
-    )
-
 copelandManipulation tieValue votes =
     let voterSet = [1..length votes]
         manipulatorSet = map (+length votes) [1..length votes + 1]
