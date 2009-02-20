51d5d06f0b50a1535ca5b1763b19845617e32c88
Chris Connett <chris@connett.net>
2009-02-20 00:16:43 -0500

    Change traceIt debug level to 0 (should only be used to chase immediate bugs).

--- Tracing.hs
+++ Tracing.hs
@@ -9,4 +9,4 @@ import Debug.Trace
 debugLevel = 0 :: Int
 myTrace :: Int -> String -> a -> a
 myTrace level = if level <= debugLevel then trace else flip const
-traceIt s = myTrace 1 ("TRACEIT:" ++ show s) s
+traceIt s = myTrace 0 ("TRACEIT:" ++ show s) s
