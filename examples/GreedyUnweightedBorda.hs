0216536cfb70f1cede4cc92102f1449e76044ecc
Chris Connett <chris@connett.net>
2011-02-01 10:26:37 -0500

    Change GreedyUnweightedBorda to use findFirst.

--- examples/GreedyUnweightedBorda.hs
+++ examples/GreedyUnweightedBorda.hs
@@ -4,7 +4,7 @@ import Data.List
 import Debug.Trace
 import Utilities
 
-greedyUnweightedBorda (c1:cn) 0 = trace (show $ c1:cn) $ all (c1>=) cn
+greedyUnweightedBorda (c1:cn) 0 = all (c1>=) cn
 greedyUnweightedBorda (c1:cn) m = greedyUnweightedBorda (c1 + (r-1) : zipWith (+) [0..r-2] (reverse $ sort cn)) (m - 1)
     where r = length cn + 1
-minGreedyUnweightedBorda scores = (length $ takeWhile (not . (greedyUnweightedBorda scores)) [0..])
+minGreedyUnweightedBorda scores = findFirst (greedyUnweightedBorda scores) [0..]
